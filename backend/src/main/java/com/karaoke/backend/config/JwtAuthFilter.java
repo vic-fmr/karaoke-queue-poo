e7e6794f3c2b22addc66db6b1cd644419d747a13
vic-fmr <victorfmarques12@gmail.com>
2025-10-22 22:17:00 -0300

    fix: non null annotations

--- backend/src/main/java/com/karaoke/backend/config/JwtAuthFilter.java
+++ backend/src/main/java/com/karaoke/backend/config/JwtAuthFilter.java
@@ -8,7 +8,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,7 +29,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final UserRepository userRepository; // Para buscar o usuário
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         var token = this.recoverToken(request);
