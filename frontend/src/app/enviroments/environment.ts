0301f3efe3d963f0d5209f64391b6aa5bcfaaf28
vic-fmr <victorfmarques12@gmail.com>
2026-06-14 15:55:45 -0300

    fix: vercel deploy

--- frontend/src/app/enviroments/environment.ts
+++ frontend/src/app/enviroments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080', // Substitua pelo SEU IP (use ipconfig)
+  apiUrl: 'https://karaoke-queue-poo.onrender.com:8080', 
 };
